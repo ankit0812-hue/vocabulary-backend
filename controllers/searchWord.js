7c5e24b2433c36de41f2f3e591ac92d2048318db
ankit0812-hue <ankitdas0812@gmail.com>
2021-09-24 11:22:21 +0530

    refactored search logic

--- controllers/searchWord.js
+++ controllers/searchWord.js
@@ -1,34 +1,30 @@
-const Word = require('../models/word');
+const Word = require("../models/word");
 
-exports.searchWord = async (req,res) =>{
-    
+exports.searchWord = async (req, res) => {
+  try {
     //Retrieving the word to be searched from the database passed as a query string
-    const word = req.query.word;
-    try {
+    const word = new RegExp(req.query.word, "i");
 
-        //Finding the word from the database and convert into simple JSON Objects
-        const result = await Word.find({word: word}).lean();
-
-        // If word exists we return the response to the client side
-        if(result.length > 0) {
-            return res.status(200).json({
-                error: false,
-                length: result.length,
-                result
-            });
-        }
-
-        // If word does not exist we send an appropriate error message
-        else {
-            return res.status(200).json({
-                error: true,
-                status: "No such word found"
-            });
-        }
+    //Finding the word from the database and convert into simple JSON Objects
+    const result = await Word.find({ word: word }).lean();
+    // If word exists we return the response to the client side
+    if (result.length > 0) {
+      return res.status(200).json({
+        error: false,
+        length: result.length,
+        result,
+      });
     }
 
+    // If word does not exist we send an appropriate error message
+    else {
+      return res.status(200).json({
+        error: true,
+        status: "No such word found",
+      });
+    }
+  } catch (error) {
     //catching any errors
-    catch(error) {
-        return res.status(500).send(error.message);
-    }
-}
+    return res.status(500).send(error.message);
+  }
+};
